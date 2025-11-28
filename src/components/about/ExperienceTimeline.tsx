4450efe551e7ff1c5a96b97e220e4ae373f87368
Harsh Vishwakarma <meeharshu8685@gmail.com>
2025-11-28 13:15:27 +0530

    Fix TypeScript error: Remove unused TimelineItem import

--- src/components/about/ExperienceTimeline.tsx
+++ src/components/about/ExperienceTimeline.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { experienceTimeline, TimelineItem } from '../../data/experienceTimeline';
+import { experienceTimeline } from '../../data/experienceTimeline';
 
 export const ExperienceTimeline = () => {
   const containerVariants = {
