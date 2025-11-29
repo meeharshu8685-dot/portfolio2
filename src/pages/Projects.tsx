a78096ceb6189fb2dd5f38425c1a0a0af23db7da
Harsh Vishwakarma <meeharshu8685@gmail.com>
2025-11-29 12:06:38 +0530

    Revert "Fix VisionOS carousel visibility - Update Projects page to use new VisionOS component - Add React imports - Center cards properly with absolute positioning - Add min-height to section"
    This reverts commit 5416f746a8f7e091f5a271b7d6366f0ec01e382b.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,15 @@
 import { AnimatedPage } from '../components/AnimatedPage';
-import ProjectsSection from '../sections/ProjectsSection';
+import { ProjectCarouselFlow } from '../components/projects/ProjectCarouselFlow';
+import { projects } from '../data/projects';
 
 export const Projects = () => {
   return (
     <AnimatedPage>
-      <ProjectsSection />
+      <section id="projects" className="min-h-screen pt-24 pb-20 transition-colors duration-300" style={{ backgroundColor: 'var(--bg-primary)' }}>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <ProjectCarouselFlow projects={projects} />
+        </div>
+      </section>
     </AnimatedPage>
   );
 };
